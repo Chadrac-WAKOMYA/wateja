fb440e32ca3e0b5879c9a660b1b7dace9b544f81
Chadrac-WAKOMYA <chadracxx@gmail.com>
2025-07-09 20:55:39 +0200

    test commit 2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <>
-      <h1 style={{display : "flex",justifyContent:"center", margin:"20px"}}>Ceci est mon permier projet  nextjs</h1>
+      <h1 style={{display : "flex",justifyContent:"center", margin:"20px", padding: "10"}}>Ceci est mon permier projet  nextjs</h1>
     </>
   );
 }
