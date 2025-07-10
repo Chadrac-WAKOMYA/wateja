a9ac564b65d012ae0a62a34738f381481342a7e2
Chadrac-WAKOMYA <chadracxx@gmail.com>
2025-07-10 12:18:32 +0200

    css .page added in page.module.css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,8 @@
-import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <div>
+    <div className={styles.page}>
       <h1>Bienvenu dans nextjs</h1>
       <a href="">Vister notre application</a>
     </div>
