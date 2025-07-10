2da73393462275fbbbd0b3e630f5f11ead765833
Chadrac-WAKOMYA <chadracxx@gmail.com>
2025-07-10 12:47:02 +0200

    Poppins google integated very well

--- src/font/font.ts
+++ src/font/font.ts
@@ -1,5 +1,5 @@
 import {Poppins} from "next/font/google";
-const poppins = Poppins({
+export const poppins = Poppins({
   weight: ["100", "200", "300", "400", "500", "600", "700"],
   subsets: ["latin"],
 });
