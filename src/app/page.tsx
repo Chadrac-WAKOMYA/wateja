d1908e427e3c6bfadbc157361ad6566306ebad97
Chadrac-WAKOMYA <chadracxx@gmail.com>
2025-07-16 19:57:46 +0200

    contacts router added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 export default function Home() { 
   return (
     <div className={styles.page}>
-      <Image src={"/hero.png"} alt="Wateja hero" width={200} height={200} />
+      {/* <Image src={"/hero.png"} alt="Wateja hero" width={200} height={200} /> */}
       <h1>Bienvenu dans nextjs</h1>
       <a href="" className="btn-link">Vister notre application</a>
     </div>
