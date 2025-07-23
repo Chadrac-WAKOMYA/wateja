8ebe8fd433bb029efa244f9e9e6777e0c68ec863
Chadrac-WAKOMYA <chadracxx@gmail.com>
2025-07-23 10:58:45 +0200

    globals.css updated

--- src/app/(contacts)/contacts/page.tsx
+++ src/app/(contacts)/contacts/page.tsx
@@ -1,9 +1,15 @@
+import Link from 'next/link'
 import React from 'react'
 
 type Props = {}
 
-export default function Contacts({}: Props) {
+export default function page({}: Props) {
   return (
-    <div>Contacts page  marche très bien</div>
+    <div className='mt-2'>
+      <header className='flex between'>
+        <h1>Liste de contacts</h1>
+        <Link href={"/add-contacts"} className='btn'>Ajouter un conact</Link>
+      </header>
+    </div>
   )
 }
