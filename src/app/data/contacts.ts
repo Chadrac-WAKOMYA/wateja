c7978022a53904be8f6f41bf2404ff67ff755da3
Chadrac-WAKOMYA <chadracxx@gmail.com>
2025-07-23 11:39:09 +0200

    contact.model.tsx updated again

--- src/app/data/contacts.ts
+++ src/app/data/contacts.ts
@@ -1,1 +1,46 @@
-export const contacts : Contacts [] = [];
+import { Contacts } from "../types/contact.model";
+
+export const contacts : Contacts [] = [
+    {
+        id : 1,
+        nom : "KIGHUNDILA",
+        email : "kighundila@gmail.com",
+        avatar : "string",
+        numTel : "0998643574"
+    },
+    {
+        id : 2,
+        nom : "MISEKA",
+        email : "miseka@gmail.com",
+        avatar : "string",
+        numTel : "0998643574"
+    },
+    {
+        id : 3,
+        nom : "BITENDANWA",
+        email : "bitendanwa@gmail.com",
+        avatar : "string",
+        numTel : "0998643574"
+    },
+    {
+        id : 4,
+        nom : "KAGOLOMBO",
+        email : "kagolombo@gmail.com",
+        avatar : "string",
+        numTel : "0998643574"
+    },
+    {
+        id : 5,
+        nom : "SALUMU",
+        email : "salumu@gmail.com",
+        avatar : "string",
+        numTel : "0998643574"
+    },
+    {
+        id : 6,
+        nom : "FATUMA",
+        email : "fatuma@gmail.com",
+        avatar : "string",
+        numTel : "0998643574"
+    }
+];
