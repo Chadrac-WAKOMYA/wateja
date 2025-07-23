5e2eda73287d8d838333154d3ce90620b09be817
Chadrac-WAKOMYA <chadracxx@gmail.com>
2025-07-23 08:51:55 +0200

    Contact style updated

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,9 +1,14 @@
+import Link from 'next/link'
 import React from 'react'
+import styles from "./page.module.css"; 
 
 type Props = {}
 
 export default function NotFound({}: Props) {
   return (
-    <div>not-found</div>
+    <div className= {styles.page}>
+        <h2>La page demandée n'existe pas</h2>
+        <Link href={"/"} className='btn-link'>Page d'accueil</Link>
+    </div>
   )
 }
