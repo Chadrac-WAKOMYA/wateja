3b742aa8adb3f17c1c4f8d103dbfef81c0563020
Chadrac-WAKOMYA <chadracxx@gmail.com>
2025-07-23 11:39:34 +0200

    Contacts model initialised

--- src/app/data/contacts.ts
+++ src/app/data/contacts.ts
@@ -5,42 +5,42 @@ export const contacts : Contacts [] = [
         id : 1,
         nom : "KIGHUNDILA",
         email : "kighundila@gmail.com",
-        avatar : "string",
+        avatar : "http://avatar.iran.liara.run/public/boy",
         numTel : "0998643574"
     },
     {
         id : 2,
         nom : "MISEKA",
         email : "miseka@gmail.com",
-        avatar : "string",
+        avatar : "http://avatar.iran.liara.run/public/girl",
         numTel : "0998643574"
     },
     {
         id : 3,
         nom : "BITENDANWA",
         email : "bitendanwa@gmail.com",
-        avatar : "string",
+        avatar : "http://avatar.iran.liara.run/public/girl",
         numTel : "0998643574"
     },
     {
         id : 4,
         nom : "KAGOLOMBO",
         email : "kagolombo@gmail.com",
-        avatar : "string",
+        avatar : "http://avatar.iran.liara.run/public/girl",
         numTel : "0998643574"
     },
     {
         id : 5,
         nom : "SALUMU",
         email : "salumu@gmail.com",
-        avatar : "string",
+        avatar : "http://avatar.iran.liara.run/public/boy",
         numTel : "0998643574"
     },
     {
         id : 6,
         nom : "FATUMA",
         email : "fatuma@gmail.com",
-        avatar : "string",
+        avatar : "http://avatar.iran.liara.run/public/girl",
         numTel : "0998643574"
     }
 ];
