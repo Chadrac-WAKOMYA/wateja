84f5eb878389c2d5fbccce70216955f4b4a3bd74
Chadrac-WAKOMYA <chadracxx@gmail.com>
2025-07-25 07:30:29 +0200

    ready to continue

--- src/app/component/contact-card.tsx
+++ src/app/component/contact-card.tsx
@@ -10,11 +10,11 @@ export default function ContactCard({contact}: Props) {
   return (
     <div key={contact.id} className='flex mt-2 between '>
         <Image alt = {"Image Loading..."} src={contact.avatar} width = {70} height = {70}/>
-        {/* <div className='flex-1'>
+        <div className='flex-1'>
             <h4>{contact.id}-{contact.nom}</h4>
             <p>{contact.email}</p>
             <p>{contact.numTel}</p>
-        </div> */}
+        </div>
         <div className='flex between'>
             <button className='btn btn-netral'>Voir</button>
             <button className='btn-danger'>Supprimer</button>
