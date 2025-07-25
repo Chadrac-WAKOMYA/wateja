4df445df140c84be82546d2ef2988e7473b2c269
Chadrac-WAKOMYA <chadracxx@gmail.com>
2025-07-25 10:02:12 +0200

    globals.css updated

--- src/app/component/add-contact-form.tsx
+++ src/app/component/add-contact-form.tsx
@@ -4,6 +4,20 @@ type Props = {}
 
 export default function AddContactForm({}: Props) {
   return (
-    <div>Contact Form</div>
+    <form action="" className='mt-2 flex'>
+        <div className="form-group">
+            <label htmlFor="nom">Nom du contact</label>
+            <input type="text" name='nom' placeholder='Nelly' id='' required minLength={3}/>
+        </div>
+        <div className="form-group">
+            <label htmlFor="email">Adresse mail du contact</label>
+            <input type="email" name='email' placeholder='Nelly' id='' required />
+        </div>
+        <div className="form-group">
+            <label htmlFor="numTel">Nom du contact</label>
+            <input type="tel" name='numTel' placeholder='Nelly' id='' required />
+        </div>
+        <button type="submit" className='btn w-full'>Ajouter le contact</button>
+    </form>
   )
 }
