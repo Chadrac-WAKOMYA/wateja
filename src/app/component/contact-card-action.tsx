05b552556acb8f5ecec7788fdb25c21597182337
Chadrac-WAKOMYA <chadracxx@gmail.com>
2025-07-25 12:14:23 +0200

    contact-card-action updated

--- src/app/component/contact-card-action.tsx
+++ src/app/component/contact-card-action.tsx
@@ -1,7 +1,8 @@
 "use client"
 import { useRouter } from 'next/navigation';
-import React from 'react';
+import React, { use } from 'react';
 import { Contact } from '../types/contact.model';
+import { ContactContext } from '../context/contact-context';
 
 type Props = {
     contact : Contact;
@@ -9,13 +10,22 @@ type Props = {
 
 export default function ContactCardAction({contact}: Props) {
     const router = useRouter();
+    const {contacts,setContact} = use(ContactContext);
+
+    const handleDeleteContact = ()=> {
+        if(confirm("Voulez-vous supprimer ce contact ?")){
+            const newContacts = contacts.filter((c)=>c.id !== contact.id);
+            setContact(newContacts);
+        }
+        
+    }
     const handleNavigate = ()=>{
         router.push(`/contacts/${contact.id}`)
     }
     return (
         <div className='flex between'>
             <button className='btn btn-netral' onClick={handleNavigate}>Voir</button>
-            <button className='btn-danger'>Supprimer</button>
+            <button className='btn-danger' onClick={handleDeleteContact}>Supprimer</button>
         </div>
     )
 }
